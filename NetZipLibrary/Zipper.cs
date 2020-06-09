71cd8fc0c4fadb129902405b51c1d3396363ded2
dbarriguete <dbarriguete@gmail.com>
2020-06-09 18:42:24 -0500

    Setting up Master Version

--- NetZipLibrary/Zipper.cs
+++ NetZipLibrary/Zipper.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace NetZipLibrary
 {
-    class Zipper
+    public class Zipper
     {
+        public void ZipFolder(string sourcePath, string targetPath)
+        {
+            sourcePath = Path.GetFullPath(sourcePath);
+            targetPath = Path.GetFullPath(targetPath);
+        }
     }
 }
